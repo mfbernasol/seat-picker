f36491cb50a663f05370fd0795d6f15147b72cea
mfbernasol <mfbernasol@gmail.com>
2021-08-22 18:33:44 -0500

    add function to save to local storage and populatUI

--- script.js
+++ script.js
@@ -5,20 +5,55 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI()
+
 let ticketPrice = +movieSelect.value;
+
+//Save Selected movie index and price 
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectedMovie',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice)
+}
 
 // update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    //copy selected seats in into array
+    // map through array
+    // return new array of indexes
+
+    const seatsIndex = [...selectedSeats].map((seat) =>  [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//get data from local storage, populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat,index)=>{
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedmovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedmovieIndex !== null){
+        movieSelect.selectedIndex = selectedmovieIndex;
+    }
+}
+
 // movie select
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.SelectedIndex,e.target.value);
     updateSelectedCount(); 
 });
 
@@ -29,3 +64,6 @@ container.addEventListener('click',(e) =>{
        updateSelectedCount();
     }
 });
+
+//initial count and total set
+updateSelectedCount();
